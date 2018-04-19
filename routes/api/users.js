d3dbe90b1a07eb414cf50d7729a1b957110f949e
TheRobOne <m_rusek@vp.pl>
2018-04-19 19:31:42 +0200

    added getting user by get route with token

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 const keys = require('../../config/keys');
 
@@ -89,6 +90,17 @@ router.post('/login', (req, res) => {
                     }
                 })
         })
+});
+
+// @route   GET api/users/current
+// @desc    return current user
+// @access  Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
 });
 
 module.exports = router;
