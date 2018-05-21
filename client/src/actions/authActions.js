9ff630f55b688c710d4a3e3eb579a1f16411fbf9
TheRobOne <m_rusek@vp.pl>
2018-05-21 23:37:53 +0200

    added logout and conditional navbar links

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -42,3 +42,14 @@ export const setCurrentUser = decoded =>{
         payload: decoded
     }
 }
+
+
+//log user out
+export const logoutUser = () => dispatch => {
+    //remove token from the local storage
+    localStorage.removeItem('jwtToken');
+    //remove auth header for future request
+    setAuthToken(false);
+    //set current user to {} which will set isAuthenticated to false
+    dispatch(setCurrentUser({}));
+}
