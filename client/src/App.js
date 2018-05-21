2c45664e99c034079770f710b244a1f646657345
TheRobOne <m_rusek@vp.pl>
2018-05-21 14:26:24 +0200

    next steps to login user

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
 
+import { setCurrentUser } from './actions/authActions';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -9,6 +12,16 @@ import Footer from './components/layout/Footer';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import store from './store';
+
+//check for token
+if(localStorage.jwtToken) {
+  //set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // set uder and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
